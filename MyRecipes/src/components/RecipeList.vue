f2037838384f2995d8c59973709b5a8f39baa3f9
Simar Keskinoglu <simar.ks@icloud.com>
2026-05-05 12:48:24 +0200

    Added RecipeList with v-for

---
+++ MyRecipes/src/components/RecipeList.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h2>Rezepte</h2>
+    <ul>
+      <li v-for="recipe in recipes" :key="recipe.id">
+        {{ recipe.name }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      recipes: [
+        { id: 1, name: "Lasagne" },
+        { id: 2, name: "Yaglama" },
+        { id: 3, name: "Pizza Diavola" }
+      ]
+    };
+  }
+};
+</script>
